e829105d9ff1a023ffa5598a02134a974cba41d0
Nate Marti <nate_marti@sil.org>
2026-09-25 11:48:44 +0100

    add facilitation script

---
+++ scripts/language-map-2026.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 -m mapper --locations --languages aiy liy bqk bfl bff bkj bdt fuu bdt gbv gbg gso gbq gbp ksp kbn lna ndy mzv mdn mdd moj mcx ngd ngg lnl nmj nzk pnz sag sqm kbn vae axk yky bjo yaj --dot-size 15 --names --image-format svg "$@"
